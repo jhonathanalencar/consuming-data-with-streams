797ee8ccf6da3f290a4c15b641a5d096ac5e0d82
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-10-31 10:41:06 -0300

    chore: getting started

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { Merriweather_Sans } from 'next/font/google';
+
+import './globals.css';
+
+const inter = Merriweather_Sans({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Webstreams',
+  description: 'Consuming data with webstreams',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}

9f869496aa7eae3202299c909dc9f4509f000900
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-11-01 19:07:55 -0300

    feat: add Navbar

---
+++ web/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { List } from '@/libs/phosphor';
+
+export function Navbar() {
+  return (
+    <header className="flex h-16 w-full items-center justify-between bg-gray-950 px-8">
+      <strong className="text-2xl font-bold tracking-tight text-emerald-200">
+        Ani<span className="font-black text-yellow-500">Stream</span>
+      </strong>
+      <nav>
+        <ul>
+          <li>
+            <List size={32} className="text-emerald-200" />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}

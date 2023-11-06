eaf21fc7595d51dcac61c6cf56996ea674fe9104
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-11-06 20:55:15 -0300

    style: increase top padding from section

--- web/src/components/Section.tsx
+++ web/src/components/Section.tsx
@@ -8,7 +8,7 @@ interface SectionRootProps extends ComponentProps<'section'> {
 function SectionRoot({ children, className, ...rest }: SectionRootProps) {
   return (
     <section
-      className={twMerge('h-full w-full bg-zinc-950 pb-16 pt-4', className)}
+      className={twMerge('h-full w-full bg-zinc-950 pb-16 pt-16', className)}
       {...rest}
     >
       {children}

57355bba6f1836903b33b28a503037d7cbb854dc
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-11-06 21:07:14 -0300

    fix: prettier compilation error

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
-    <footer className="w-full">
-      <div className="bg-gradient-footer bg-zinc-950 p-8">
+    <footer className="w-full bg-zinc-950">
+      <div className="bg-gradient-footer p-8">
         <div className="container mx-auto flex items-center justify-center border-t-2 border-zinc-800 py-2">
           <p className="mt-1 text-base font-medium text-zinc-400">
             © AniStream
